c54e79cc2782a8df2768509d0601a0c17977b686
mkwasniewski <empty@empty.com>
2019-01-30 00:12:20 +0100

    wordReverter

---
+++ src/main/java/pl/sda/tests/reverter/WordReverter.java
@@ -0,0 +1,25 @@
+package pl.sda.tests.reverter;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.StringJoiner;
+import java.util.stream.Collectors;
+
+public class WordReverter {
+
+    public String revert(String value){
+        StringBuffer stringBuffer = new StringBuffer(value);
+        return stringBuffer.reverse().toString();
+    }
+
+    public String revertOnlyLetterInWords(String value){
+        List<String> wordsList = Arrays.asList(value.split(("\\s+")));
+        List<String> wordsListRev = wordsList.stream().map(n -> revert(n)).collect(Collectors.toList());
+        StringJoiner joiner = new StringJoiner(" ");
+        for (String word : wordsListRev) {
+            joiner.add(word);
+        }
+        return joiner.toString();
+    }
+
+}
